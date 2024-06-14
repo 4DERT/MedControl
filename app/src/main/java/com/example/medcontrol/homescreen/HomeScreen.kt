9a3cf1036552e74ab2f84fa373f4fa042099a503
4DERT <4dertdk@gmail.com>
2024-06-14 18:41:08 +0200

    Removed temporary comment

--- app/src/main/java/com/example/medcontrol/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/medcontrol/homescreen/HomeScreen.kt
@@ -62,31 +62,6 @@ fun HomeScreen() {
     val context = LocalContext.current
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
-    ///// TEMPORARY
-
-//    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-//    val intent = Intent(context, AlarmReceiver::class.java)
-//
-//    val pendingIntent = PendingIntent.getBroadcast(
-//        context,
-//        0,
-//        intent,
-//        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-//    )
-//
-//    val calendar = Calendar.getInstance().apply {
-//        set(Calendar.HOUR_OF_DAY, 19)
-//        set(Calendar.MINUTE, 57)
-//        set(Calendar.SECOND, 0)
-//        if (before(Calendar.getInstance())) {
-//            add(Calendar.DAY_OF_MONTH, 1)
-//        }
-//    }
-//
-//    alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
-
-    //////
-
     // creating database
     val db by lazy {
         Room.databaseBuilder(
