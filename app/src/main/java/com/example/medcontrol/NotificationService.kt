fbee5f77ad26390d1c10e700ba02c3a28df3c4af
4DERT <4dertdk@gmail.com>
2024-06-15 13:06:34 +0200

    Add opening app when clicking on notificcation

--- app/src/main/java/com/example/medcontrol/NotificationService.kt
+++ app/src/main/java/com/example/medcontrol/NotificationService.kt
@@ -2,21 +2,31 @@ package com.example.medcontrol
 
 import android.app.Application
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 
 class NotificationService(
-    private val context: Context
+    private val context: Context,
 ) {
 
     private val notificationManager = context.getSystemService(Application.NOTIFICATION_SERVICE) as NotificationManager
 
     fun showNotification(title: String, text: String, id: Int? = null) {
+        // Create an intent to launch the main activity when the notification is clicked
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
         val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentText(text)
             .setContentTitle(title)
+            .setContentIntent(pendingIntent)  // Set the pending intent here
             .setOnlyAlertOnce(true)
+            .setAutoCancel(true)  // Automatically remove the notification when clicked
             .build()
 
         notificationManager.notify(
